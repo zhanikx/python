7283ea941c176536487c1b96ae56138caeac8dc8
zhanx <kurmanov.1999@bk.ru>
2022-02-24 13:56:37 +0600

    второй пз

---
+++ pb2-1.py
@@ -0,0 +1,3 @@
+list_1 = ['aaaa', 17, True, 55.6, {1, 2, 3, 4}]
+for i in range(len(list_1)):
+    print(type(list_1[i]))
---
+++ pb2-4.py
@@ -0,0 +1,3 @@
+n = input('vvedite ').split( )
+for i in range(len(n)):
+    print(i + 1, n[i][0:10])
